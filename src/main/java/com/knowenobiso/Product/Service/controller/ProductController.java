35213fed52b463ce30c938a620133298ac73bf32
Knowen Obiso <knowenemmanuel@gmail.com>
2023-06-23 16:44:07 +0300

    Created Singring boot microservices for Produce Service connecting with MongoDB hosted online

---
+++ src/main/java/com/knowenobiso/Product/Service/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.knowenobiso.Product.Service.controller;
+
+import com.knowenobiso.Product.Service.dto.ProductRequest;
+import com.knowenobiso.Product.Service.dto.ProductResponse;
+import com.knowenobiso.Product.Service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor //to inject product service
+public class ProductController {
+    private  final ProductService productService;
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest); //creating the product by calling the product service
+    }
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts()
+    {
+        return productService.getAllProducts();
+    }
+
+
+}
